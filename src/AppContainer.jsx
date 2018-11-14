521b7269e39f931ba9c51e7b587df75c0e179735
Satvik Gadag <satvik.g@HCL.COM>
2018-11-14 21:33:27 +0530

    Settings Skin chooser WIP

---
+++ src/AppContainer.jsx
@@ -0,0 +1,84 @@
+import { MuiThemeProvider } from "@material-ui/core";
+import blue from "@material-ui/core/colors/blue";
+import pink from "@material-ui/core/colors/pink";
+import red from "@material-ui/core/colors/red";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme } from "@material-ui/core/styles";
+import React, { Component } from "react";
+import autoBind from "react-autobind";
+import { Provider } from "react-redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+import App from "./App";
+import * as reducers from "./store/reducers";
+
+const store = createStore(combineReducers(reducers), applyMiddleware(thunk));
+
+// All the following keys are optional.
+// We try our best to provide a great default value.
+const defaultLightTheme = {
+  palette: {
+    primary: blue,
+    secondary: pink,
+    error: red,
+    type: "light",
+    // Used by `getContrastText()` to maximize the contrast between the background and
+    // the text.
+    contrastThreshold: 3,
+    // Used to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2
+  }
+};
+
+// All the following keys are optional.
+// We try our best to provide a great default value.
+const defaultDarkTheme = {
+  palette: {
+    primary: blue,
+    secondary: pink,
+    error: red,
+    type: "dark",
+    // Used by `getContrastText()` to maximize the contrast between the background and
+    // the text.
+    contrastThreshold: 3,
+    // Used to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2
+  }
+};
+
+class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+    this.state = {};
+    this.theme = createMuiTheme(defaultLightTheme);
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.handleSetTheme(defaultDarkTheme);
+    }, 10000);
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider theme={this.theme}>
+        <CssBaseline />
+        <Provider store={store}>
+          <App setTheme={this.handleSetTheme} />
+        </Provider>
+      </MuiThemeProvider>
+    );
+  }
+
+  handleSetTheme(themeOptions) {
+    this.theme = createMuiTheme(themeOptions);
+    this.forceUpdate();
+  }
+}
+
+export default AppContainer;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import { Provider } from "react-redux";
-import "./index.css";
 import "typeface-roboto";
-import App from "./App";
+import AppContainer from "./AppContainer";
+import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
-import * as reducers from "./store/reducers";
-const store = createStore(combineReducers(reducers), applyMiddleware(thunk));
-
-ReactDOM.render(
-  <React.Fragment>
-    <CssBaseline />
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.Fragment>,
-  document.getElementById("root")
-);
+ReactDOM.render(<AppContainer />, document.getElementById("root"));
 registerServiceWorker();
